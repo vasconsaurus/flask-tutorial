feb3e28b10d22f56ae9ef77a052b3015a4414328
manu vasconcelos <manu.vasconcelos@gmail.com>
2023-02-02 16:28:00 -0300

    bug: my create function was written created

--- flaskr/blog.py
+++ flaskr/blog.py
@@ -12,14 +12,14 @@ bp = Blueprint("blog", __name__)
 def index():
     db = get_db()
     posts = db.execute("SELECT p.id, title, body, created, author_id, username"
-                       "FROM post p JOIN user u ON p.author_id = u.id"
-                       "ORDER BY created DESC").fetchall()
+                       " FROM post p JOIN user u ON p.author_id = u.id"
+                       " ORDER BY created DESC").fetchall()
     return render_template("blog/index.html", posts=posts)
 
 
 @bp.route("/create", methods=("GET", "POST"))
 @login_required
-def created():
+def create():
     if request.method == "POST":
         title = request.form["title"]
         body = request.form["body"]
